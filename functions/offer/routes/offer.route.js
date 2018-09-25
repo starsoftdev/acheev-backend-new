9514239a8c5391fe3fad1702c21cbcd61ce99e6e
starsoftdev <26078620+starsoftdev@users.noreply.github.com>
2018-09-24 23:57:40 -0400

    Add endpoint for uploading thumbnails for offer

--- functions/offer/routes/offer.route.js
+++ functions/offer/routes/offer.route.js
@@ -11,5 +11,6 @@ router.delete('/:id', OfferController.destroy);
 
 router.get('/user/:user_id', OfferController.listByUser);
 router.post('/user/:user_id', OfferController.create);
+router.post('/user/:user_id/thumbnails', OfferController.uploadThumbnails);
 
 export default router;
